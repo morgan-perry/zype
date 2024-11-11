c39218bb63f3954888f5c7eb7e02f9d7a1d981b5
Morgan Perry <morganperry@posteo.net>
2024-11-11 17:40:11 +1000

    Updated main.zig to be client.zig for clarity

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "client",
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/client.zig"),
         .target = target,
         .optimize = optimize,
     });
